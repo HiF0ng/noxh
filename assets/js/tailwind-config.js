5ef6c978660af03fc55c8f135e65195d4ea40bcf
Bui Hai Phong <95003661+HiF0ng@users.noreply.github.com>
2026-07-29 17:22:04 +0700

    Commit 17h20 29/7/2026
    Hoàn thành bộ nhận diện

--- assets/js/tailwind-config.js
+++ assets/js/tailwind-config.js
@@ -83,14 +83,14 @@ tailwind.config = {
                 "label-md": ["Be Vietnam Pro", "sans-serif"]
             },
             "fontSize": {
-                "label-sm": ["12px", { "lineHeight": "16px", "fontWeight": "500" }],
-                "display": ["48px", { "lineHeight": "56px", "letterSpacing": "-0.02em", "fontWeight": "700" }],
-                "headline-lg-mobile": ["28px", { "lineHeight": "36px", "fontWeight": "700" }],
-                "body-md": ["16px", { "lineHeight": "24px", "fontWeight": "400" }],
-                "headline-lg": ["32px", { "lineHeight": "40px", "letterSpacing": "-0.01em", "fontWeight": "700" }],
-                "body-lg": ["18px", { "lineHeight": "28px", "fontWeight": "400" }],
-                "headline-md": ["24px", { "lineHeight": "32px", "fontWeight": "600" }],
-                "label-md": ["14px", { "lineHeight": "20px", "letterSpacing": "0.01em", "fontWeight": "600" }]
+                "label-sm": ["11px", { "lineHeight": "16px", "fontWeight": "500" }],
+                "display": ["40px", { "lineHeight": "48px", "letterSpacing": "-0.02em", "fontWeight": "700" }],
+                "headline-lg-mobile": ["24px", { "lineHeight": "32px", "fontWeight": "700" }],
+                "body-md": ["14px", { "lineHeight": "20px", "fontWeight": "400" }],
+                "headline-lg": ["28px", { "lineHeight": "36px", "letterSpacing": "-0.01em", "fontWeight": "700" }],
+                "body-lg": ["16px", { "lineHeight": "24px", "fontWeight": "400" }],
+                "headline-md": ["20px", { "lineHeight": "28px", "fontWeight": "600" }],
+                "label-md": ["13px", { "lineHeight": "18px", "letterSpacing": "0.01em", "fontWeight": "600" }]
             }
         }
     },
